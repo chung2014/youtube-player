57dda1591a1653779d83f9f099852b084e846f63
chung2014 <chung2014@gmail.com>
2020-01-19 00:53:04 +0800

    Initial Commit

---
+++ MyYoutube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyYoutube
+//
+//  Created by Man Chung Wong on 19/1/2020.
+//  Copyright © 2020 Man Chung Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
